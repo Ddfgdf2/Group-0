e3844071f4edf2344a10bad1c40e2a086ac5365f
Ddfgdf2 <ddfgdf6@yahoo.com>
2024-06-02 11:49:58 +0400

    Add files via upload

---
+++ Level 2/level 2.py
@@ -0,0 +1,11 @@
+# მომხმარებელს უნდა შეეკითხოთ რამდენი წლისაა
+# და რამდენი წლით უნდა დროში მოგზაურობა
+
+# შემდეგ უნდა დაპრინტოთ რამდენი წლის იქნება დროში მოგზაურობის შემდეგ.
+# და sololearn Input and output ჩათვლით ყველაფერი
+
+
+gela =int(input("ramdeni wlis xar?: "))
+gela2 =int(input("ramdeni wlit ginda droshi mogzauroba?: "))
+gela3=gela+gela2
+print("droshi mogzaurobis shemdeg shen iqnebi: ", gela3, "wlis")
